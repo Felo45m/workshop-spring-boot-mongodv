4c87bebda22cdee28dc174d3331bd9efc08ad34f
Felo <felipemelo713@gmail.com>
2023-04-11 14:54:59 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/felo/workshopmongo/config/Instantiation.java
@@ -0,0 +1,33 @@
+package com.felo.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.felo.workshopmongo.domain.User;
+import com.felo.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+	
+	
+
+}
--- src/main/java/com/felo/workshopmongo/domain/User.java
+++ src/main/java/com/felo/workshopmongo/domain/User.java
@@ -1,14 +1,12 @@
 package com.felo.workshopmongo.domain;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
 public class User implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -17,10 +15,10 @@ public class User implements Serializable {
 	private String email;
 	
 	public User() {
-		
 	}
 
 	public User(String id, String name, String email) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -52,7 +50,10 @@ public class User implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -64,6 +65,11 @@ public class User implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 }
