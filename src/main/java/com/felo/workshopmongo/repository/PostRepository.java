30526d22c9871d0da2540c631160497027871217
Felo <felipemelo713@gmail.com>
2023-04-13 19:39:09 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/felo/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.felo.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.felo.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+	
+}
